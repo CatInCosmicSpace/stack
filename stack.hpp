73a8f632744983923f6df5bea8c58f47f50f563c
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-09-19 11:18:29 +0300

    Task 4
    /*noexcept*/
    /*strong*/
    /*basic*/

--- stack.hpp
+++ stack.hpp
@@ -10,24 +10,24 @@ using std::ostream;
 template<typename T>
 class stack {
 public:
-	stack();
-	stack(stack const & rhs);
-	~stack();
+	stack(); /*noexcept*/
+	stack(stack const & rhs); /*strong*/
+	~stack(); /*noexcept*/
 
-	auto count() const noexcept->size_t;
-	auto top() const->const T&;
-	auto pop() -> void;
-	auto push(T const & value) -> void;
+	auto count() const noexcept -> size_t; /*noexcept*/
+	auto top() const -> const T&; /*strong*/
+	auto pop() -> void; /*strong*/
+	auto push(T const & value) -> void; /*strong*/
 
-	auto operator=(stack const & rhs)->stack &;
-	auto operator==(stack const & rhs) -> bool;
+	auto operator=(stack const & rhs) -> stack &; /*noexcept*/
+	auto operator==(stack const & rhs) -> bool; /*noexcept*/
 private:
 	T * array_;
 	size_t array_size_;
 	size_t count_;
 
-	auto copy(const T * rhs, size_t sizeLeft, size_t sizeRight)->T *;
-	auto swap(stack & rhs) -> void;
+	auto copy(const T * rhs, size_t sizeLeft, size_t sizeRight)->T *; /*strong*/
+	auto swap(stack & rhs) -> void; /*noexcept*/
 };
 
 #include "stack.cpp"
