20e3592ed554e99827b803d431e5598d57d32945
Alena Egorova <anon5007@gmail.com>
2016-09-21 18:12:37 +0300

    Added task 4 to tests-branch

--- include/stack.cpp
+++ include/stack.cpp
@@ -8,22 +8,14 @@ inline stack<T>::stack() : count_(0) {
 }
 
 template<typename T>
-inline stack<T>::stack(stack const & rhs) :	array_size_(rhs.array_size_),
-											count_(rhs.count_),
-											array_(new T[rhs.array_size_]) {
-	if (count_ > 0) {
-		T * m_array = new T[array_size_];
-		std::copy(rhs.array_, rhs.array_ + rhs.array_size_, m_array);
-		if (array_) {
-			delete[] array_;
-		}
-		array_ = m_array;
-	}
+inline stack<T>::stack(stack const & rhs) : array_size_(rhs.array_size_),
+count_(rhs.count_),
+array_(copy(rhs.array_, rhs.count_, rhs.array_size_)) {
 }
 
 template<typename T>
 inline stack<T>::~stack() {
-		delete[] array_;
+	delete[] array_;
 }
 
 template<typename T>
@@ -32,14 +24,17 @@ inline auto stack<T>::count() const noexcept -> size_t {
 }
 
 template<typename T>
-auto stack<T>::top() const -> T {
+auto stack<T>::top() const -> const T& {
+	if (count_ == 0) {
+		throw std::range_error("stack is empty");
+	}
 	return array_[count_ - 1];
 }
 
 template<typename T>
 inline auto stack<T>::pop() -> void {
 	if (count_ == 0) {
-		throw std::logic_error("stack is empty!");
+		throw std::logic_error("stack is empty");
 	}
 	--count_;
 }
@@ -48,14 +43,13 @@ template<typename T>
 inline auto stack<T>::push(T const & value) -> void {
 	if (count_ == array_size_) {
 		size_t size = array_size_ * 2 + (array_size_ == 0);
-		T * m_array = new T[size];
-		std::copy(array_, array_ + array_size_, m_array);
+		T * newArray = copy(array_, count_, size);
 		delete[] array_;
-		array_ = m_array;
+		array_ = newArray;
 		array_size_ = size;
 	}
+	array_[count_] = value;
 	++count_;
-	array_[count_ - 1] = value;
 }
 
 template<typename T>
@@ -82,18 +76,22 @@ auto stack<T>::operator==(stack const & rhs) -> bool {
 }
 
 template<typename T>
+auto stack<T>::copy(const T * rhs, size_t sizeLeft, size_t sizeRight) -> T * {
+	T * m_array = new T[sizeRight];
+	try {
+		std::copy(rhs, rhs + sizeLeft, m_array);
+	}
+	catch (...) {
+		delete[] m_array;
+		throw;
+	}
+	return m_array;
+}
+
+template<typename T>
 auto stack<T>::swap(stack & rhs) -> void {
 	std::swap(rhs.array_size_, array_size_);
 	std::swap(rhs.array_, array_);
 	std::swap(rhs.count_, count_);
 }
-
-/*template <typename T>
-ostream & operator<<(ostream & output, stack<T> const & stack) {
-	for (size_t i = 0; i < stack.count_; ++i) {
-		output << stack.array_[i] << " ";
-	}
-	output << std::endl;
-	return output;
-}*/
 #endif // STACK_CPP
--- include/stack.hpp
+++ include/stack.hpp
@@ -1,40 +1,33 @@
 #include <cstdlib>
 #include <iostream>
 #include <memory>
-//#include <cassert>
 #ifndef STACK_HPP
 #define STACK_HPP
 
 using std::size_t;
 using std::ostream;
 
-/*template<typename T>
-class stack;
-
-template<typename T>
-ostream & operator<<(ostream & output, stack<T> & stack);*/
-
 template<typename T>
 class stack {
 public:
-	stack(); // TESTED
-	stack(stack const & rhs); // TESTED
-	~stack(); 
+	stack(); /*noexcept*/
+	stack(stack const & rhs); /*strong*/
+	~stack(); /*noexcept*/
 
-	auto count() const noexcept -> size_t; // TESTED
-	auto top() const -> T; // TESTED
-	auto pop() -> void; // TESTED
-	auto push(T const & value) -> void; // TESTED
+	auto count() const noexcept -> size_t; /*noexcept*/
+	auto top() const -> const T&; /*strong*/
+	auto pop() -> void; /*strong*/
+	auto push(T const & value) -> void; /*strong*/
 
-	auto operator=(stack const & rhs) -> stack &; // TESTED
-	auto operator==(stack const & rhs) -> bool; // TESTED
-	//friend ostream & operator<< <>(ostream & output, stack<T> const & stack);
+	auto operator=(stack const & rhs) -> stack &; /*noexcept*/
+	auto operator==(stack const & rhs) -> bool; /*noexcept*/
 private:
 	T * array_;
 	size_t array_size_;
 	size_t count_;
 
-	auto swap(stack & rhs) -> void;
+	auto copy(const T * rhs, size_t sizeLeft, size_t sizeRight)->T *; /*strong*/
+	auto swap(stack & rhs) -> void; /*noexcept*/
 };
 
 #include "stack.cpp"
