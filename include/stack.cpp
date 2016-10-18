dda88f764c05d0b987d7b52e563b071b4084b9fc
Alena Egorova <anon5007@gmail.com>
2016-10-19 01:42:09 +0300

    Fixed errors
    Fixed issues #9, #10, #11

--- include/stack.cpp
+++ include/stack.cpp
@@ -10,20 +10,20 @@
 //////////////////////////////////////////////
 
 template<typename T> /*noexcept*/
-inline ::allocator<T>::allocator(size_t size) :
+inline allocator<T>::allocator(size_t size) :
 	ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))),
 	size_(size),
 	count_(0) {
 }
 
 template<typename T> /*noexcept*/
-inline ::allocator<T>::~allocator() {
-	destroy(ptr_, ptr_ + size_);
+inline allocator<T>::~allocator() {
+	//destroy(ptr_, ptr_ + size_);
 	operator delete(ptr_);
 }
 
 template<typename T> /*noexcept*/
-auto ::allocator<T>::swap(allocator & other) -> void {
+auto allocator<T>::swap(allocator & other) -> void {
 	std::swap(ptr_, other.ptr_);
 	std::swap(count_, other.count_);
 	std::swap(size_, other.size_);
@@ -43,57 +43,60 @@ inline stack<T>::stack() :
 template<typename T> /*strong*/
 inline stack<T>::stack(stack const & rhs) :
 	allocator<T>(rhs.size_) {
-	for (size_t i = allocator<T>::count_; i < rhs.count_; ++i) {
-		construct(allocator<T>::ptr_ + i, rhs.ptr_[i]);
+	for (size_t i = count_; i < rhs.count_; ++i) {
+		construct(ptr_ + i, rhs.ptr_[i]);
 	}
-	allocator<T>::count_ = rhs.count_;
+	count_ = rhs.count_;
 }
 
 template<typename T> /*noexcept*/
 inline stack<T>::~stack() {
+	destroy(ptr_);
 }
 
 template<typename T> /*noexcept*/
 inline auto stack<T>::count() const noexcept -> size_t {
-	return allocator<T>::count_;
+	return count_;
 }
 
 template<typename T> /*noexcept*/
-auto stack<T>::empty() const noexcept -> bool {
-	return (allocator<T>::count_ == 0);
+inline auto stack<T>::empty() const noexcept -> bool {
+	return (count_ == 0);
 }
 
 template<typename T> /*strong*/
-auto stack<T>::top() const -> const T&{
-	if (allocator<T>::count_ == 0) {
+inline auto stack<T>::top() const -> const T&{
+	if (count_ == 0) {
 		throw std::range_error("stack is empty");
 	}
 	else {
-		return allocator<T>::ptr_[allocator<T>::count_ - 1];
+		return ptr_[count_ - 1];
 	}
 }
 
 template<typename T> /*strong*/
 inline auto stack<T>::pop() -> void {
-	if (allocator<T>::count_ == 0) {
+	if (count_ == 0) {
 		throw std::logic_error("stack is empty");
 	}
 	else {
-		--allocator<T>::count_;
+		destroy(ptr_ + count_);
+		--count_;
 	}
 }
 
 template<typename T> /*strong*/
 inline auto stack<T>::push(T const & value) -> void {
-	if (allocator<T>::count_ == allocator<T>::size_) {
-		size_t size = allocator<T>::size_ * 2 + (allocator<T>::size_ == 0);
-		T * newArray = copy(allocator<T>::ptr_, allocator<T>::count_, size);
-		delete[] allocator<T>::ptr_;
-		allocator<T>::ptr_ = newArray;
-		allocator<T>::size_ = size;
+	if (count_ == size_) {
+		size_t size = size_ * 2 + (size_ == 0);
+		T * newArray = copy(ptr_, count_, size);
+		delete[] ptr_;
+		ptr_ = newArray;
+		size_ = size;
 	}
-	allocator<T>::ptr_[allocator<T>::count_] = value;
-	++allocator<T>::count_;
+	construct(ptr_ + count_, value);
+	//ptr_[count_] = value;
+	++count_;
 }
 
 template<typename T> /*strong*/
@@ -105,13 +108,13 @@ inline auto stack<T>::operator=(stack const & rhs) -> stack & {
 }
 
 template<typename T> /*noexcept*/
-auto stack<T>::operator==(stack const & rhs) -> bool {
-	if ((rhs.count_ != allocator<T>::count_) || (rhs.size_ != allocator<T>::size_)) {
+inline auto stack<T>::operator==(stack const & rhs) -> bool {
+	if ((rhs.count_ != count_) || (rhs.size_ != size_)) {
 		return false;
 	}
 	else {
-		for (size_t i = 0; i < allocator<T>::count_; i++) {
-			if (rhs.ptr_[i] != allocator<T>::ptr_[i]) {
+		for (size_t i = 0; i < count_; i++) {
+			if (rhs.ptr_[i] != ptr_[i]) {
 				return false;
 			}
 		}
@@ -120,7 +123,7 @@ auto stack<T>::operator==(stack const & rhs) -> bool {
 }
 
 template<typename T>  /*strong*/
-auto copy(const T * rhs, size_t sizeLeft, size_t sizeRight) -> T * {
+inline auto copy(const T * rhs, size_t sizeLeft, size_t sizeRight) -> T * {
 	T * m_array = new T[sizeRight];
 	try {
 		std::copy(rhs, rhs + sizeLeft, m_array);
@@ -133,17 +136,17 @@ auto copy(const T * rhs, size_t sizeLeft, size_t sizeRight) -> T * {
 }
 
 template <typename T1, typename T2>
-auto construct(T1 * ptr, T2 const & value) -> void {
+inline auto construct(T1 * ptr, T2 const & value) -> void {
 	new(ptr) T1(value);
 }
 
 template <typename T>
-auto destroy(T * ptr) noexcept -> void {
+inline auto destroy(T * ptr) noexcept -> void {
 	ptr->~T();
 }
 
 template <typename FwdIter>
-auto destroy(FwdIter first, FwdIter last) noexcept -> void {
+inline auto destroy(FwdIter first, FwdIter last) noexcept -> void {
 	for (; first != last; ++first) {
 		destroy(&*first);
 	}
