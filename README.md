24174c059558d95f0edba32cbf513045ab8f9878
Alena Egorova <anon5007@gmail.com>
2016-10-20 19:01:16 +0300

    Make it

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# magic_box
-
-[![Build Status](https://travis-ci.org/simplydesigner/magic_box.svg?branch=master)](https://travis-ci.org/simplydesigner/magic_box)
-
-```bash
-tools/build.sh #{PROJECT_NAME}
-tools/test.sh
-tools/clean.sh
-```
+[![Build Status](https://travis-ci.org/TAHK6e3TOPMO3OB/STACK.svg?branch=master)](https://travis-ci.org/TAHK6e3TOPMO3OB/STACK)
--- tests/init.cpp
+++ tests/init.cpp
@@ -1,59 +1,54 @@
-#include "stack.hpp"
+#include <stack.cpp>
 #include <catch.hpp>
+#include <iostream>
+using namespace std;
 
-SCENARIO("Stack: init", "[init]") {
-	stack<size_t> a;
-	// object is created and memory allocated for it
-	REQUIRE(sizeof(a) != 0);
+SCENARIO("count", "[count]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
 }
 
-SCENARIO("Stack: operator==", "[op==]") {
-	stack<size_t> a, b;
-	a.push(1);
-	b.push(1);
-	REQUIRE(a == b);
+SCENARIO("push", "[push]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+  REQUIRE(s.top()==1);
 }
 
-SCENARIO("Stack: copy", "[copy]") {
-	stack<size_t> a;
-	a.push(1);
-	stack<size_t> b = a;
-	REQUIRE(a == b);
+SCENARIO("pop", "[pop]"){
+  stack<int> s;
+  s.push(1); s.pop();
+  REQUIRE(s.count()==0);
 }
 
-SCENARIO("Stack: count", "[count]") {
-	stack<size_t> a;
-	a.push(1);
-	a.push(2);
-	REQUIRE(a.count() == 2);
+SCENARIO("prisv", "[prisv]"){
+  stack<int> s;
+  s.push(1);
+  stack<int> s2;
+  s2=s;
+  REQUIRE(s.count()==1);
+  REQUIRE(s.top()==1);
 }
 
-SCENARIO("Stack: top", "[top]") {
-	stack<size_t> a;
-	a.push(1);
-	a.push(2);
-	REQUIRE(a.top() == 2);
+SCENARIO("cop", "[cop]"){
+  stack<int> s;
+  s.push(1);
+  stack<int> s2=s;
+  REQUIRE(s2.count()==1);
+  REQUIRE(s2.top()==1);
 }
 
-SCENARIO("Stack: pop", "[pop]") {
-	stack<size_t> a;
-	a.push(1);
-	a.push(2);
-	a.pop();
-	REQUIRE(a.count() == 1);
+SCENARIO("top", "[top]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.top()==1);
 }
 
-SCENARIO("Stack: push", "[push]") {
-	stack<size_t> a;
-	a.push(1);
-	REQUIRE(a.top() == 1);
-	REQUIRE(a.count() == 1);
+SCENARIO("empty", "[empty]"){
+  stack<int> s1, s2;
+  s1.push(1);
+  REQUIRE(!s1.empty());
+  REQUIRE(s2.empty());
 }
 
-SCENARIO("Stack: operator=", "[op=]") {
-	stack<size_t> a;
-	a.push(1);
-	stack<size_t> b;
-	b = a;
-	REQUIRE(b == a);
-}
